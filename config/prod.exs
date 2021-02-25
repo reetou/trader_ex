7d2f96c283a22512343943ad23ca631607a8fbdf
reetou <fillmasterson@gmail.com>
2021-02-25 16:27:21 +0300

    Remove duplicate

--- config/prod.exs
+++ config/prod.exs
@@ -1,11 +1,5 @@
 use Mix.Config
 
-config :trader, Trader.Repo,
-  database: "trader_prod",
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost"
-
 config :tinkoff_invest,
   token: "mytoken",
   broker_account_id: "mybroker",
